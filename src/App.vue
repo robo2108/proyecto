e641db5dde3fa7cb52e3bea2b5704ceed887800b
unknown <radg@kth.se>
2023-05-31 16:48:18 -0600

    archivos inicio

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+const sliderValue = ref(50);
+</script>
+
+<template>
+  <header class="header">
+    Control
+  </header>
+  <div class="custom-slider">
+    <input v-model="sliderValue" type="number" class="input" />
+    <br>
+    <input
+      v-model="sliderValue"
+      type="range"
+      min="0"
+      max="100"
+      class="slider"
+    />
+   
+  </div>
+
+</template>
+<style scoped>
+
+
+</style>
