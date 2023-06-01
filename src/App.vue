d9314bcf955786c94044a901a07db8e5de4a2879
unknown <radg@kth.se>
2023-05-31 21:09:00 -0600

    botones

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <script setup>
 import { ref } from "vue";
 const sliderValue = ref(50);
+const direccion = ref(0);
+
+
 </script>
 
+
+
 <template>
-  <header class="header">
-    Control
-  </header>
+  <header class="header">Control</header> 
+  <button v-on:click="direccion= 0">Stop</button>
+  <button v-on:click="direccion= 1">Forward</button>
+  <button v-on:click="direccion= -1">Reverse</button>
+
+  <p>Direccion {{ direccion }}</p>
   <div class="custom-slider">
-    <input v-model="sliderValue" type="number" class="input" />
-    <br>
-    <input
-      v-model="sliderValue"
-      type="range"
-      min="0"
-      max="100"
-      class="slider"
-    />
-   
-  </div>
+  <input v-model="sliderValue" type="number" />
+  <br />
+  <input v-model="sliderValue" type="range" min="0" max="100" class="slider" />
 
+
+</div>
 </template>
-<style scoped>
 
-
-</style>
